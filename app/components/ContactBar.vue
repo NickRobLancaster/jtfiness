e9fe9b1ddb62844dc158ad2a944c575a51fbbfd8
Nick Lancaster <nickroblancaster@gmail.com>
2025-11-30 01:48:43 -0800

    updated contact bar

--- app/components/ContactBar.vue
+++ app/components/ContactBar.vue
@@ -3,13 +3,17 @@
 <template>
   <div class="flex flex-row items-center gap-2">
     <a
-      href="mailto:info@ieharmreduction.org"
-      class="btn max-sm:btn-sm btn-secondary"
+      href="mailto:3jtfitness3@gmail.com"
+      class="btn max-sm:btn-sm btn-secondary text-white"
     >
       Email Us
     </a>
-    <!-- <a href="tel:180088880000" @click="" class="btn max-sm:btn-sm btn-accent">
+    <a
+      href="tel:79513851941"
+      @click=""
+      class="btn max-sm:btn-sm btn-accent text-white"
+    >
       Call Us
-    </a> -->
+    </a>
   </div>
 </template>
